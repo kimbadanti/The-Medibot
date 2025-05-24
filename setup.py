3472d63c64682b5e1f7f179ef506d194f5c23a04
kimbadanti <shaw08kimbadanti@gmail.com>
2025-05-24 16:12:45 +0530

    template added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Kimbadanti Shaw',
+    author_email= 'shaw08kimbadanti@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
